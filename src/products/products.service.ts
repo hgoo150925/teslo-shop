d7a1b93c2c83874be49e41f425af5224bd3217cc
hugoorlando <hugo.orlando2030@gmail.com>
2022-08-15 21:29:20 -0300

    create image in db

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -12,7 +12,7 @@ import { validate as isUUID } from 'uuid';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Product } from './entities/product.entity';
+import { Product, ProductImage } from './entities/index';
 
 @Injectable()
 export class ProductsService {
@@ -20,17 +20,26 @@ export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
+
+    @InjectRepository(ProductImage)
+    private readonly productImageRepository: Repository<ProductImage>,
   ) {}
 
   async create(createProductDto: CreateProductDto) {
     try {
+      const { images = [], ...productDetails } = createProductDto;
       // Creamos el registro
-      const product = this.productRepository.create(createProductDto);
+      const product = this.productRepository.create({
+        ...productDetails,
+        images: images.map((image) =>
+          this.productImageRepository.create({ url: image }),
+        ),
+      });
 
       //Guardamos el registro en la DB
       await this.productRepository.save(product);
 
-      return product;
+      return { ...product, image: images };
     } catch (error) {
       this.handleDBExceptions(error);
     }
@@ -71,6 +80,7 @@ export class ProductsService {
     const product = await this.productRepository.preload({
       id: id,
       ...updateProductDto,
+      images: [],
     });
 
     if (!product) {
